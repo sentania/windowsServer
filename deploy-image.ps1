34bbc99fa993a643385b3d84d5ab64648ca1a2b8
Scott R. Bowe <scottb@sentania.net>
2020-07-19 14:22:52 -0500

    Hardcoded Content Library

--- deploy-image.ps1
+++ deploy-image.ps1
@@ -8,13 +8,13 @@ Connect-VIServer -Server vcenter.int.sentania.net -User $vSphereUSERNAME -Passwo
 
 $sourceVMobj = get-vm -Name $sourceVM
 
-Export-VM -Destination C:\temp\ -Format Ova -VM $sourceVMobj
+Export-VM -Destination /tmp/ -Format Ova -VM $sourceVMobj -Name server2019.ova
 
 $contentlibraryitem = @{
     ContentLibrary = Get-ContentLibrary -name "sentania.net"
-    File = get-item -path "/tmp/packertest.ova"
+    File = get-item -path "/tmp/server2019.ova"
     name = "packertest"
 }
 New-ContentLibraryItem @contentlibraryitem
-
+Remove-Item /tmp/server2019.ova -Confirm:$false
 #simple update.
