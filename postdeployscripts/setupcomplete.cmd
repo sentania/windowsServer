9bd41d0c098b632e5a1b5573ddb4e57b0b7b803c
Scott R. Bowe <scottb@sentania.net>
2020-07-19 16:32:20 -0500

    wrong iso location

--- postdeployscripts/setupcomplete.cmd
+++ postdeployscripts/setupcomplete.cmd
@@ -10,8 +10,8 @@ echo %PS%
 
 REM Call the SetupComplete.ps1 script, to begin configuring the server
 md C:\temp
-echo [%date% %time%] Setupcomplete.cmd starting... > C:\temp\nm-setup.log
-echo [%date% %time%] Calling setupcomplete.ps1 >> C:\temp\nm-setup.log
+echo [%date% %time%] Setupcomplete.cmd starting... > C:\temp\lab-setup.log
+echo [%date% %time%] Calling setupcomplete.ps1 >> C:\temp\lab-setup.log
 %PS% set-executionpolicy unrestricted
 
 start %PS% %windir%\setup\scripts\setupcomplete.ps1
