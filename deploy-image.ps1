6570be3ed1b12959abed7bb8258cfeb31da115be
Scott Bowe <scottb@sentania.net>
2020-07-21 16:01:57 -0500

    Build

--- deploy-image.ps1
+++ deploy-image.ps1
@@ -9,12 +9,3 @@ Connect-VIServer -Server vcenter.int.sentania.net -User $vSphereUSERNAME -Passwo
 $sourceVMobj = get-vm -Name $sourceVM
 
 Export-VM -Destination /tmp/ -Format Ova -VM $sourceVMobj -Name server2019
-
-$contentlibraryitem = @{
-    ContentLibrary = Get-ContentLibrary -name "vcenter.int.sentania.net"
-    File = get-item -path "/tmp/server2019.ova"
-    name = "server2019"
-}
-New-ContentLibraryItem @contentlibraryitem
-Remove-Item /tmp/server2019.ova -Confirm:$false
-#simple update.
